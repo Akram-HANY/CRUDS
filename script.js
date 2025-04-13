a3796d5240226a905905f2b21d352745368f3432
Akram-HANY <ibnhany595@gmail.com>
2025-04-14 00:00:45 +0200

    Release

--- script.js
+++ script.js
@@ -3,53 +3,69 @@ let price = document.getElementById("price");
 let taxes = document.getElementById("taxes");
 let ads = document.getElementById("ads");
 let discount = document.getElementById("discount");
-let total = document.getElementById("total");
 let count = document.getElementById("count");
 let category = document.getElementById("category");
+let total = document.getElementById("total");
 let submit = document.getElementById("submit");
-
-console.log(title, price, taxes, ads, discount, total, count, category, submit);
+let mood = "create";
+let tmp;
 //get total
-
 function getTotal() {
   if (price.value != "") {
-    let result = +price.value + +taxes.value + +ads.value - discount.value;
+    let result = +price.value + +taxes.value + +ads.value - +discount.value;
     total.innerHTML = result;
     total.style.background = "#040";
   } else {
     total.innerHTML = "";
     total.style.background = "#a00d02";
   }
-
-  console.log("getTotal function called");
 }
-
 //create product
 let dataPro;
-
-if (localStorage.products != null) {
-  dataPro = JSON.parse(localStorage.products);
+if (localStorage.product != null) {
+  dataPro = JSON.parse(localStorage.product);
 } else {
   dataPro = [];
 }
 
 submit.onclick = function () {
   let newPro = {
-    title: title.value,
+    title: title.value.toLowerCase(),
     price: price.value,
     taxes: taxes.value,
     ads: ads.value,
     discount: discount.value,
     total: total.innerHTML,
     count: count.value,
-    category: category.value,
+    category: category.value.toLowerCase(),
   };
-  dataPro.push(newPro);
-  localStorage.setItem("products", JSON.stringify(dataPro));
+
+  //count
+  if(title.value != '' && price.value != '' && category.value != ''){
+     if (mood === "create") {
+    if (newPro.count > 1) {
+      for (let i = 0; i < newPro.count; i++) {
+        dataPro.push(newPro);
+      }
+    } else {
+      dataPro.push(newPro);
+    }
+  } else {
+    dataPro[tmp] = newPro;
+    mood = "create";
+    submit.innerHTML = "Create";
+    count.style.display = "block";
+  }
   clearData();
-  showData();
+
+  }
+ 
+
+  //save localstorage
+  localStorage.setItem("product", JSON.stringify(dataPro));
+  console.log(dataPro);
+   showData();
 };
-//save local storage
 //clear inputs
 function clearData() {
   title.value = "";
@@ -61,88 +77,138 @@ function clearData() {
   count.value = "";
   category.value = "";
 }
-
 //read
-
 function showData() {
+  getTotal();
   let table = "";
   for (let i = 0; i < dataPro.length; i++) {
-    table += `   <tr>
-            <td>${i}</td>
-            <td>${dataPro[i].title}</td>
-            <td>${dataPro[i].price}</td>
-            <td>${dataPro[i].taxes}</td>
-            <td>${dataPro[i].ads}</td>
-            <td>${dataPro[i].discount}</td>
-            <td>${dataPro[i].total}</td>
-            <td>${dataPro[i].count}</td>
-            <td>${dataPro[i].category}</td>
-             <td><button id="update" class="update">Update</button></td>
-            <td><button onclick="deleteData(${i})" id="delete" class="delete">Delete</button></td>
-          </tr>
-          `;
-  }
-  document.getElementById("tbody").innerHTML = table;
+    table += `
+       <tr>
+              <td>${i+1}</td>
+              <td>${dataPro[i].title}</td>
+              <td>${dataPro[i].price}</td>
+              <td>${dataPro[i].taxes}</td>
+              <td>${dataPro[i].ads}</td>
+              <td>${dataPro[i].discount}</td>
+              <td>${dataPro[i].total}</td>
+              <td>${dataPro[i].count}</td>
+              <td>${dataPro[i].category}</td>
 
+              <td><button onclick="updateData(${i})"  id="update">update</button></td>
+              <td><button onclick="deleteData(${i})" id="delete">delete</button></td>
+
+             </tr>
+   `;
+  }
+
+  document.getElementById("tbody").innerHTML = table;
   let btnDelete = document.getElementById("deleteAll");
   if (dataPro.length > 0) {
-    btnDelete.innerHTML = `<button onclick="deleteAll()">Delete All</button>`;
-  }else{
+    btnDelete.innerHTML = `
+                  <button onclick="deleteAll()">Delete All(${dataPro.length})</button>
+
+    `;
+  } else {
     btnDelete.innerHTML = "";
   }
 }
-
 showData();
-
-//count
 //delete
 function deleteData(i) {
   dataPro.splice(i, 1);
-  localStorage.products = JSON.stringify(dataPro);
+  localStorage.product = JSON.stringify(dataPro);
   showData();
 }
-// Check if required fields are not empty
-
-submit.onclick = function () {
-  if (
-    title.value.trim() !== "" &&
-    price.value.trim() !== "" &&
-    category.value.trim() !== ""
-  ) {
-    let newPro = {
-      title: title.value,
-      price: price.value,
-      taxes: taxes.value,
-      ads: ads.value,
-      discount: discount.value,
-      total: total.innerHTML,
-      count: count.value,
-      category: category.value,
-    };
-    // Handle count
-    if (newPro.count > 1) {
-      for (let i = 0; i < newPro.count; i++) {
-        dataPro.push(newPro);
-      }
-    } else {
-      dataPro.push(newPro);
-    }
-
-    localStorage.setItem("products", JSON.stringify(dataPro));
-    clearData();
-    showData();
-  } else {
-    alert("Please fill Empty Fields!!!");
-  }
-};
-
 //delete all
-function deleteAll(){
-   localStorage.clear();
-   dataPro.splice(0);
-   clearData();
-   showData();
+function deleteAll() {
+  localStorage.clear();
+  dataPro.splice(0);
+  showData();
 }
+
 //update
+function updateData(i) {
+  title.value = dataPro[i].title;
+  price.value = dataPro[i].price;
+  taxes.value = dataPro[i].taxes;
+  ads.value = dataPro[i].ads;
+  getTotal();
+  count.style.display = "none";
+  category.value = dataPro[i].category;
+  submit.innerHTML = "Update";
+  mood = "Update";
+  tmp = i;
+  scroll({
+    top: 0,
+    behavior: "smooth",
+  });
+}
 //search
+let searchMood = "title";
+
+function getSearchMood(id) {
+  let search = document.getElementById("search");
+  if (id == "searchTitle") {
+    searchMood = "title";
+    search.placeholder = "search By Title";
+  } else {
+    searchMood = "category";
+    search.placeholder = "search By Category";
+  }
+  search.focus();
+  search.value = "";
+  showData();
+}
+
+function searchData(value) {
+  let table = "";
+  if (searchMood == "title") {
+    for (let i = 0; i < dataPro.length; i++) {
+      if (dataPro[i].title.includes(value.toLowerCase())) {
+        table += `
+       <tr>
+              <td>${i}</td>
+              <td>${dataPro[i].title}</td>
+              <td>${dataPro[i].price}</td>
+              <td>${dataPro[i].taxes}</td>
+              <td>${dataPro[i].ads}</td>
+              <td>${dataPro[i].discount}</td>
+              <td>${dataPro[i].total}</td>
+              <td>${dataPro[i].count}</td>
+              <td>${dataPro[i].category}</td>
+
+              <td><button onclick="updateData(${i})"  id="update">update</button></td>
+              <td><button onclick="deleteData(${i})" id="delete">delete</button></td>
+
+             </tr>
+   `;
+      }
+    }
+  } else {
+    for (let i = 0; i < dataPro.length; i++) {
+      if (dataPro[i].category.includes(value.toLowerCase())) {
+        table += `
+           <tr>
+                  <td>${i}</td>
+                  <td>${dataPro[i].title}</td>
+                  <td>${dataPro[i].price}</td>
+                  <td>${dataPro[i].taxes}</td>
+                  <td>${dataPro[i].ads}</td>
+                  <td>${dataPro[i].discount}</td>
+                  <td>${dataPro[i].total}</td>
+                  <td>${dataPro[i].count}</td>
+                  <td>${dataPro[i].category}</td>
+    
+                  <td><button onclick="updateData(${i})"  id="update">update</button></td>
+                  <td><button onclick="deleteData(${i})" id="delete">delete</button></td>
+    
+                 </tr>
+       `;
+      }
+    }
+  }
+  document.getElementById("tbody").innerHTML = table;
+}
+
 //clean data
+
